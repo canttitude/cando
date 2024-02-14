994d820c349826647c6a8470756ab1fe9a8df1ac
paulbalan <paulbalan@users.noreply.github.com>
2024-02-14 15:01:33 +0000

    Fix styling

--- src/CandoServiceProvider.php
+++ src/CandoServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Canttitude\Cando;
 
+use Canttitude\Cando\Commands\CandoCommand;
+use Canttitude\Cando\Testing\TestsCando;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Canttitude\Cando\Commands\CandoCommand;
-use Canttitude\Cando\Testing\TestsCando;
 
 class CandoServiceProvider extends PackageServiceProvider
 {
